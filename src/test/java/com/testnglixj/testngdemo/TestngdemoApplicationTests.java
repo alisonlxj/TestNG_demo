aa938df8a5d9f029bc16783079967b47898511f9
alison <lixianjun2087@163.com>
2019-08-24 17:44:11 +0800

    first commit to new a repository

---
+++ src/test/java/com/testnglixj/testngdemo/TestngdemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.testnglixj.testngdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestngdemoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
