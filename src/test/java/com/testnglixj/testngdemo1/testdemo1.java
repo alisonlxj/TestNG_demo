3a65f82ea9d01a3c58736dff502ec0c9ebb5e282
youyouing <yuemeiling@163.com>
2019-08-25 22:01:26 +0800

    配置了testng相关

---
+++ src/test/java/com/testnglixj/testngdemo1/testdemo1.java
@@ -0,0 +1,14 @@
+package com.testnglixj.testngdemo1;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestDemo1{
+
+    @Test
+    public void test1(){
+        Assert.assertEquals(1,1);
+    }
+
+
+}
